6494e88e290451d73c532fac0d48044e69d80a75
Wes Weirather <weirdev@outlook.com>
2019-04-02 06:29:38 -0500

    Added support for graphing3d's plot3d_scatter function.

--- src/py3dgraph.py
+++ src/py3dgraph.py
@@ -1,5 +1,6 @@
 import os
-from ctypes import cdll
+import numpy as np
+from ctypes import cdll, c_double, c_uint
 
 if os.name == 'nt':
     graphing3d = cdll.LoadLibrary(r"../graphing3dlib/graphing3d")
@@ -8,3 +9,11 @@ elif os.name == 'posix':
     graphing3d = cdll.LoadLibrary(r"../graphing3dlib/libgraphing3d.so")
 
 subroutine1 = graphing3d.subroutine1
+_plot3d_scatter_internal = graphing3d.plot3d_scatter
+
+def plot3d_scatter(points: np.ndarray):
+    pointcount = c_uint(points.shape[0])
+    points = points.flatten()
+    BUFTYPE = c_double * len(points)
+    buffer = BUFTYPE(*points)
+    _plot3d_scatter_internal(buffer, pointcount)
--- src/test.py
+++ src/test.py
@@ -1,5 +1,12 @@
+import numpy as np
 import py3dgraph
 
+def scatter():
+    pointcoords = (np.random.rand(3*20) * 0.8) + 0.1
+    points = pointcoords.reshape((20, 3))
+    py3dgraph.plot3d_scatter(points)
+
 if __name__ == '__main__':
-    py3dgraph.subroutine1()
-    
+    #py3dgraph.subroutine1()
+    scatter()
+    pass
