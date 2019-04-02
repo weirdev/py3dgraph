0d6e35eec2e636bef318ea9816e194b11387eb76
Wes Weirather <weirdev@outlook.com>
2019-04-02 06:52:19 -0500

    Improved scatter plotting performance by eliminating a slow copy of data passed to graphing3d.

--- src/py3dgraph.py
+++ src/py3dgraph.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from ctypes import cdll, c_double, c_uint
+from ctypes import cdll, c_double, c_uint, POINTER
 
 if os.name == 'nt':
     graphing3d = cdll.LoadLibrary(r"../graphing3dlib/graphing3d")
@@ -12,8 +12,12 @@ subroutine1 = graphing3d.subroutine1
 _plot3d_scatter_internal = graphing3d.plot3d_scatter
 
 def plot3d_scatter(points: np.ndarray):
+    """Generate a scatterplot image. Note this function
+    operates directly on the memory storing `points`, so 
+    `points` should be copied if it is to be used after
+    this call."""
     pointcount = c_uint(points.shape[0])
-    points = points.flatten()
-    BUFTYPE = c_double * len(points)
-    buffer = BUFTYPE(*points)
-    _plot3d_scatter_internal(buffer, pointcount)
+    # TODO: force data to be laid out in row major order
+    c_double_ptr = POINTER(c_double)
+    beffore_ptr = points.ctypes.data_as(c_double_ptr)
+    _plot3d_scatter_internal(beffore_ptr, pointcount)
